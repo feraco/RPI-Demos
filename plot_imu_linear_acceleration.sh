d14c5d245873e988bf2636bec79d6c84f098a662
Fred Feraco <frederick.feraco@gmail.com>
2024-12-19 06:19:01 -0500

    Create plot_imu_linear_acceleration.sh

---
+++ plot_imu_linear_acceleration.sh
@@ -0,0 +1,1 @@
+ros2 run rqt_plot rqt_plot /ros_robot_controller/imu_raw/linear_acceleration/x /ros_robot_controller/imu_raw/linear_acceleration/y /ros_robot_controller/imu_raw/linear_acceleration/z
